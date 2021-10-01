a786727c74087eea040817e8a1f45527d51d3018
Mahsa <mahsa.alikhany@gmail.com>
2021-10-01 22:40:32 +0330

    create addNewAccount() method in Bank class.

--- src/ir/bank/Bank.java
+++ src/ir/bank/Bank.java
@@ -8,4 +8,20 @@ public class Bank {
     Customer[] customers = new Customer[1000];
     static int customerIndex = 0;
 
+    public void addNewAccount(String name, long id, double openingFund, MyDate openingDate) {
+        for (int i = 0; i < 1000; i++) {
+            if (customers[i].getNationalId() == id) {
+                System.out.println("This account exists before!");
+            } else {
+                customers[customerIndex] = new Customer();
+                customers[customerIndex].setName(name);
+                customers[customerIndex].setNationalId(id);
+                customers[customerIndex].addNewAccountBank(openingDate, openingFund);
+                customerIndex++;
+                System.out.println("New account is created successfully");
+                break;
+            }
+
+        }
+    }
 }
--- src/ir/bank/Management.java
+++ src/ir/bank/Management.java
@@ -7,22 +7,7 @@ public class Management {
 
 
 
-    public void addNewAccount(String name, long id, double openingFund, MyDate openingDate) {
-        for (int i = 0; i < 1000; i++) {
-            if (customers[i].getNationalId() == id) {
-                System.out.println("This account exists before!");
-            } else {
-                customers[customerIndex] = new Customer();
-                customers[customerIndex].setName(name);
-                customers[customerIndex].setNationalId(id);
-                customers[customerIndex].addNewAccountBank(openingDate, openingFund);
-                customerIndex++;
-                System.out.println("New account is created successfully");
-                break;
-            }
 
-        }
-    }
 
     public void printCustomerAccount(long id) {
         for (int i = 0; i < customerIndex; i++) {
